b4812cebe14e60712de466bcb9f379acd0268170
dhanushn77 <dhanushn2608@gmail.com>
2024-09-26 07:03:05 +0530

    Add files via upload

---
+++ code1.py
@@ -0,0 +1,14 @@
+#simple print statement
+print("hello world\nhello world")
+print("Dhanush"+ " "+"N")
+
+#task after debugging
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+
+
