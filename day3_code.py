71123a2d19e34b8096efdc4862d78f22393bc4b0
dhanushn77 <dhanushn2608@gmail.com>
2024-10-04 20:32:33 +0530

    Update day3_code.py

--- day3_code.py
+++ day3_code.py
@@ -1,125 +1,125 @@
-# print("Welcome to the rollercoaster!")
-# height = int(input("What is your height in cm? "))
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
 
-# if height>=120:
-#     print("You can Ride")
-# else:
-#     print("Sorry still need to grow")
+if height>=120:
+    print("You can Ride")
+else:
+    print("Sorry still need to grow")
 
 
-# #to check the given number is odd or not    
+#to check the given number is odd or not    
     
-# # 🚨 Don't change the code below 👇
-# number = int(input("Which number do you want to check? "))
-# # 🚨 Don't change the code above 👆
+# 🚨 Don't change the code below 👇
+number = int(input("Which number do you want to check? "))
+# 🚨 Don't change the code above 👆
 
-# #Write your code below this line 👇
-# if number%2==0:
-#     print("the given no is even")
-# else:
-#         print("the given no is odd")
-
-
-#         # 🚨 Don't change the code below 👇
-# height = float(input("enter your height in m: "))
-# weight = float(input("enter your weight in kg: "))
-# # 🚨 Don't change the code above 👆
-
-# #Write your code below this line 👇
-# bmi=int(weight/(height*height))
-# print(bmi)
-# if bmi<=18.5:
-#     print("underweight")
-# elif bmi<=25:
-#     print("normal weight")
-# elif bmi<=30:
-#     print("slightly overweight")
-# elif bmi<=35:
-#     print("obese")
-# else:
-#     print("clinically obese.")
+#Write your code below this line 👇
+if number%2==0:
+    print("the given no is even")
+else:
+        print("the given no is odd")
 
 
-# # 🚨 Don't change the code below 👇
-# year = int(input("Which year do you want to check? "))
-# # 🚨 Don't change the code above 👆
+        # 🚨 Don't change the code below 👇
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+# 🚨 Don't change the code above 👆
 
-# #Write your code below this line 👇
-
-# if year%4==0:
-#     if year%100==0:
-#         if year%400==0:
-#             print("leap year")
-#         else:
-#             print("not leap year")
-
-#     else:
-#         print("leap year")
-
-# else:
-#     print("not a leap year")
-
+#Write your code below this line 👇
+bmi=int(weight/(height*height))
+print(bmi)
+if bmi<=18.5:
+    print("underweight")
+elif bmi<=25:
+    print("normal weight")
+elif bmi<=30:
+    print("slightly overweight")
+elif bmi<=35:
+    print("obese")
+else:
+    print("clinically obese.")
 
 
-# # 🚨 Don't change the code below 👇
-# print("Welcome to Python Pizza Deliveries!")
-# size = input("What size pizza do you want? S, M, or L ")
-# add_pepperoni = input("Do you want pepperoni? Y or N ")
-# extra_cheese = input("Do you want extra cheese? Y or N ")
-# # 🚨 Don't change the code above 👆
+# 🚨 Don't change the code below 👇
+year = int(input("Which year do you want to check? "))
+# 🚨 Don't change the code above 👆
 
-# #Write your code below this line 👇S
-# A=0
-# B=0
-# C=0
-# cost_of_small=15
-# cost_of_midium=20
-# cost_of_large=25
-# if size=="S" :
-#     A=A+cost_of_small
-# elif size=="M":
-#     B=B+cost_of_midium
-# elif size=="L":
-#     C=C+cost_of_large
-# else:
-#     print("invalid entry")
-# if (add_pepperoni=='Y'and extra_cheese=='N'):
-#     if size=="S":
-#         A=A+2
-#         print("the total cost would be",A)
-#     elif (size=="M" and extra_cheese=='N') :
-#         B=B+3
-#         print("the total cost would be",B)
-#     elif (size=="L"and extra_cheese=='N' ):
-#         C=C+3
-#         print("the total cost would be",C)
+#Write your code below this line 👇
+
+if year%4==0:
+    if year%100==0:
+        if year%400==0:
+            print("leap year")
+        else:
+            print("not leap year")
+
+    else:
+        print("leap year")
+
+else:
+    print("not a leap year")
+
+
+
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇S
+A=0
+B=0
+C=0
+cost_of_small=15
+cost_of_midium=20
+cost_of_large=25
+if size=="S" :
+    A=A+cost_of_small
+elif size=="M":
+    B=B+cost_of_midium
+elif size=="L":
+    C=C+cost_of_large
+else:
+    print("invalid entry")
+if (add_pepperoni=='Y'and extra_cheese=='N'):
+    if size=="S":
+        A=A+2
+        print("the total cost would be",A)
+    elif (size=="M" and extra_cheese=='N') :
+        B=B+3
+        print("the total cost would be",B)
+    elif (size=="L"and extra_cheese=='N' ):
+        C=C+3
+        print("the total cost would be",C)
 
             
-# if (add_pepperoni=='N'and extra_cheese=='N'):
-#     if size=="S":
+if (add_pepperoni=='N'and extra_cheese=='N'):
+    if size=="S":
         
-#         print("the total cost would be",A)
-#     elif (size=="M"and extra_cheese=='N') :
+        print("the total cost would be",A)
+    elif (size=="M"and extra_cheese=='N') :
         
-#         print("the total cost would be",B)
-#     elif (size=="L"or extra_cheese=='N' ):
+        print("the total cost would be",B)
+    elif (size=="L"or extra_cheese=='N' ):
         
-#         print("the total cost would be",C)
+        print("the total cost would be",C)
 
 
-# if(add_pepperoni=="Y" and extra_cheese=='Y'):
-#     if size=="S":
-#         A=A+1
-#         A=A+2
-#         print("the total cost would be",A)
-#     elif size=="M":
-#         B=B+3    
-#         B=B+1
-#         print("the total cost would be",B)
-#     elif size=="L":
-#         C=C+1
-#         C=C+3
-#         print("the total cost would be",C)
+if(add_pepperoni=="Y" and extra_cheese=='Y'):
+    if size=="S":
+        A=A+1
+        A=A+2
+        print("the total cost would be",A)
+    elif size=="M":
+        B=B+3    
+        B=B+1
+        print("the total cost would be",B)
+    elif size=="L":
+        C=C+1
+        C=C+3
+        print("the total cost would be",C)
 
 
 
