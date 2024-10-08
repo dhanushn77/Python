b21fb4c6e4b78f612ed07844451a8a6b9f20dbc8
dhanushn77 <dhanushn2608@gmail.com>
2024-10-08 17:58:28 +0530

    Add files via upload

---
+++ day6_code.py
@@ -0,0 +1,135 @@
+
+#defining the user defined functions and using them
+# def turn_around():
+#     turn_left()
+#     turn_left()
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# turn_left()  
+# move()
+# turn_right()  
+# move()
+# turn_right() 
+# move()
+# turn_right()
+# move()
+#--------------------------------------------------------------------------------------------------------
+#hurdle challenge
+# def turn_around():
+#     turn_left()
+#     turn_left()
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():    
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+# for i in range(0,6):              
+#     jump()
+#
+
+##    or
+# num_of_herdle=6
+# while num_of_herdle>0:
+#     jump()
+#     num_of_herdle-=1
+    
+#     print(num_of_herdle)
+#------------------------------------------------------------------------------------------
+
+#while loop chalenges
+# def turn_around():
+#     turn_left()
+#     turn_left()
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump():    
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# while at_goal()!=True:
+#     jump()
+# print("goal reached")
+ #-----------------------------------------------------------------------------------------------  
+   #hurdle challenge
+# def turn_around():
+#     turn_left()
+#     turn_left()
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def jump(): 
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     while front_is_clear():
+#         move()
+#     turn_left()
+
+ 
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+#____________________________________________________________________________________________________
+# def turn_around():
+#     turn_left()
+#     turn_left()
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+        
+#     elif front_is_clear():
+#         move()
+        
+#     else:
+#         turn_left()
+   
+   
+   
+   
+            
+        
+
+        
+
+                        
+          
+        
+
+        
+
+                        
+          
+
+
+
+
